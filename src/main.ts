f673ce56dee10879c4a14f7a6c324411657aa4f1
DANG-PH <dangph.ptit@gmail.com>
2025-10-05 13:20:44 +0700

    sss

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+      origin: '*', // hoặc chỉ định domain cụ thể
+  });
+
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
