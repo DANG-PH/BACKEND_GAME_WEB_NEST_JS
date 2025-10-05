fb2fb8d64293e2e9549ecde893f35a4da3e8ad05
DANG-PH <dangph.ptit@gmail.com>
2025-10-05 20:19:43 +0700

    fix utf8

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({ origin: '*' });
+
+  // Parse JSON với UTF-8
+  app.use(bodyParser.json({ limit: '10mb', type: 'application/json' }));
 
   // ✅ Railway cung cấp PORT qua biến môi trường
   const port = process.env.PORT ? Number(process.env.PORT) : 8080;
