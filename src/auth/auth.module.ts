b914da4f98fc212b6f13ba75c458ee19fd801c7c
DANG-PH <dangph.ptit@gmail.com>
2025-10-06 10:43:35 +0700

    Fix

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'secretKey', // key bí mật
       signOptions: { expiresIn: '1h' },            // token sống 1h
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy, JwtAuthGuard],
   exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
